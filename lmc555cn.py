8f02cf1e639ed335fed1887e90a69a5097d9f84d
umedoblock <devnull@localhost>
2013-01-02 21:14:55 +0900

    refactoring. (refs #216, #215, #208)

--- lmc555cn.py
+++ lmc555cn.py
@@ -15,28 +15,6 @@ from engineer_number.constants import *
 # 3.0 # 2 ** 3 = 8, pow(2, 3)
 CONST = math.log(2, math.e)
 
-# c = 0.1 * MICRO
-
-# tH = 0.693 * (ra + rb) * c
-# tL = 0.693 * rb * c
-# t = tH + tL
-#   = 0.693 * (ra + 2 * rb) * c
-# t = 0.693 * 3 * r * c if r = ra = rb
-
-# t = 0.001 # 1 kHz
-# = 0.693 * (ra + 2 * rb) * c
-# = 0.693 * (ra + 2 * rb) * c = 0.001
-# c = 0.001 / (0.693 * (ra + 2 * rb))
-
-# if r = ra = rb = 1 kR
-# c = 0.001 / (0.693 * (1k + 2 * 1k))
-#   = 0.001 / (0.693 * 3k )
-# >>> 0.001 / (0.693 * 3000 )
-# 4.81000481000481e-07
-# 0.481000481000481e-06
-# 0.481 * 10 ** -6
-# 0.481 * MICRO
-
 def lmc555(Ra, Rb, C=EngineerNumber(0.1*MICRO)):
     _c2gnd = 0.1 * MICRO
     tH = CONST * (Ra + Rb) * C
@@ -45,28 +23,6 @@ def lmc555(Ra, Rb, C=EngineerNumber(0.1*MICRO)):
     f = 1 / t
 
     return (tL, tH, t, f)
-
-'''
-手持ちの抵抗は最大で 10M R
-手持ちの抵抗は最小で 1 R
-t = 0.693 * (ra + 2 * rb) * c
-ここで、抵抗部分を rx として、
-t = 0.693 * rx * c
-制限があるのは、cの値なので、まずcの値を適当に決める。
-"・０．１μＦ５０Ｖ
-積層セラミックコンデンサ
-（ムラタ製または各社相当品）"
-
-積セラ 50V 0.1uF 2.54ピッチ[R指]
-積セラ 50V 0.1uF 5.08ピッチ[R指]
-積セラ 50V 1uF[R指]
-
-LMC555 CMOS タイマ
-■ 5V 電源で1mW 以下の消費電力
-■ 3MHzまでの無安定周波数
-■ 1.5V 動作電源電圧の保証
-■ 出力は5V 電源で、TTL、CMOS ロジックと完全互換
-'''
 
 def _make_all_combination(series='E12'):
     if series != 'E12':
@@ -103,26 +59,13 @@ def look_for_optimized_Hz(Hz, c=EngineerNumber(0.1, MICRO)):
     print('len_combination =', len_combination)
 
     tf = []
-    condition = False
     for r2 in Rs:
         for r1 in Rs:
             rb = EngineerNumber(r2, ONE)
             ra = EngineerNumber(r1, ONE)
             tL, tH, t, f = lmc555(ra, rb, c)
-            condition = 1 or t == f and t == 1
             tup = (tL, tH, t, f, ra, rb, c) # sort() x index refer here.
             tf.append(tup)
-          # if condition:
-          #     print('--')
-          #     print('ra = {:.3e}'.format(ra))
-          #     print('rb = {:.3e}'.format(rb))
-          #     print('c = {:.3e}'.format(c))
-          #     print()
-
-          #     print('tL = {:.3e}'.format(tL))
-          #     print('tH = {:.3e}'.format(tH))
-          #     print('t = {:.3e}'.format(t))
-          #     print('f = {:.3e}'.format(f))
 
     # Hz を優先して sort。
     tf.sort(key=lambda x: math.fabs(Hz - x[3]))
