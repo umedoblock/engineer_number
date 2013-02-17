3da2ac052dd88ed7e7f74c7eb4baca8df7bac1c2
umedoblock <devnull@localhost>
2013-02-17 14:32:48 +0900

    言語の優先順位を決定する為の script.

--- decide_language.py
+++ decide_language.py
@@ -8,21 +8,24 @@ def get_default_languages():
             continue
         sp = env.split(':')
         languages.extend(sp)
-    languages.append('C')
+    if not languages:
+        languages.append('C')
     return languages
 
 def decide_languages_order(hope=[]):
+    # first is overwrited by second.
+    # so should be hope=['lower-important', 'higher-important']
     languages = get_default_languages()
     if 'ja' not in languages:
         languages.insert(0, 'zannenenglish')
-    if hope:
-        languages = hope + languages
+    languages.extend(hope)
 
     return languages
 
 print(get_default_languages())
 print(decide_languages_order())
-print(decide_languages_order(hope=['foo', 'bar']))
+print(decide_languages_order(hope=['lower-important', 'higher-important']))
+# $ export LANGUAGE=en:ja
 # ['en', 'ja_JP.UTF-8', 'C']
 # ['zannenenglish', 'en', 'ja_JP.UTF-8', 'C']
 # ['foo', 'bar', 'zannenenglish', 'en', 'ja_JP.UTF-8', 'C']
