ffdd1ddd8aa0711b90763b0606c44b5ef9c5b231
umedoblock <umedoblock@umetaro>
2017-09-28 22:34:54 +0900

    余計な debug log を出力しないようにした。

--- engineer_number/scripts/emitter_amplifier.py
+++ engineer_number/scripts/emitter_amplifier.py
@@ -64,8 +64,9 @@ def look_for_optimized_gain(gain, ic, Vcc, hfe=200, e_series="E12"):
         Rb2_ = close_e_series(Rb2, "up", e_series, TOLERANCE_ERROR) # E12 にあわす
         Rb1_ = close_e_series(Rb1, "up", e_series, TOLERANCE_ERROR) # E12 にあわす
         if (not Rb2_) or (not Rb1_):
-            print("Rb2={}, Rb2_={}, Rb2.error(Rb2_)={}".format(Rb2, Rb2_, Rb2.error(Rb2_)))
-            print("Rb1={}, Rb1_={}, Rb1.error(Rb1_)={}".format(Rb1, Rb1_, Rb1.error(Rb1_)))
+#           print("--")
+#           print("Rb2={}, Rb2_={}".format(Rb2, Rb2_))
+#           print("Rb1={}, Rb1_={}".format(Rb1, Rb1_))
             continue
         Radd = Rb1_ + Rb2_
       # print("Radd =", Radd)
