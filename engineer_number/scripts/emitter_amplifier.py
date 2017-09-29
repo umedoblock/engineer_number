37208a20eb62bae53a447988e026b3ad97f90840
umedoblock <umedoblock@umetaro>
2017-09-29 12:01:57 +0900

    common-emitter amplifier の全数検索を出来るようにした。

--- engineer_number/scripts/emitter_amplifier.py
+++ engineer_number/scripts/emitter_amplifier.py
@@ -10,6 +10,66 @@ from engineer_number.constants import *
 from engineer_number.lib import get_resistors, close_e_series
 
 NAMES = ("Rb1", "Rb2", "Rc", "Re", "gain", "Vcc", "Vcb", "Vb", "Vc", "Ve", "Vce", "ie", "ic", "ib", "ibias", "Pce")
+
+def _emitter_common_amplifier(Vcc, ic, hfe, Rc, Re, e_series):
+    d = {}
+    ib = ic / hfe # ?
+    ie = ic + ib
+    Vc = Rc * ic   # 6
+    Ve = Re * ie
+
+    # 注意
+    # must be Vc + Ve <= Vcc
+    if Vc + Ve > Vcc:
+      # raise ValueError("invalid Vc and Ve combination compare Vcc.")
+      # print("added(Vc={}, Ve={})={} is greater ghan Vcc={}.".format(Vc, Ve, Vc + Ve, Vcc))
+        return None
+
+    Vce = Vcc - (Vc + Ve) # 7
+    gain = Vc / Ve
+    Pce = Vce * ic        # 8
+    # Pce は小さきこと。
+    # 2SC1815 の場合，
+    # Pce < 400mW(=PD)
+
+    Vb = Ve + 0.6 # 9
+    Vcb = Vcc - Vb
+
+    # hfe = 200 # 10
+    ibias = 10 * ib # 11
+    Rb2 = Vb / ibias # 12
+    Rb1 = Vcb / ibias # 13
+    Rb2_ = close_e_series(Rb2, "down", e_series)
+    Rb1_ = close_e_series(Rb1, "down", e_series)
+    Rb2, Rb1 = Rb2_, Rb1_
+    if (not Rb2) or (not Rb1):
+#       print("--")
+#       print("Rb2={}, Rb2_={}".format(Rb2, Rb2_))
+#       print("Rb1={}, Rb1_={}".format(Rb1, Rb1_))
+        return None
+    Radd = Rb1 + Rb2
+    ibias = Vcc / Radd
+
+    for name in NAMES:
+        d[name] = locals()[name]
+#   print("d =")
+#   print(d)
+    return d
+
+def brute_force_to_look_for_gain_and_Pce(Vcc, ic, hfe, e_series):
+    resistors = get_resistors(e_series)
+    parameters = [None] * len(resistors) ** 2
+
+    i = 0
+    for Rc in resistors:
+        for Re in resistors:
+            d = _emitter_common_amplifier(Vcc, ic, hfe, Rc, Re, e_series)
+            if d:
+                parameters[i] = d
+                i += 1
+
+    return parameters[:i]
+
 def look_for_optimized_gain(gain, ic, Vcc, hfe=200, e_series="E12"):
     resistors = get_resistors(e_series)
 
@@ -126,7 +186,9 @@ if __name__ == '__main__':
     hfe = args.hfe
     e_series = args.e_series
 
-    parameters = look_for_optimized_gain(gain, ic, Vcc, hfe, e_series)
+    parameters = brute_force_to_look_for_gain_and_Pce(Vcc, ic, hfe, e_series)
+#   parameters = look_for_optimized_gain(gain, ic, Vcc, hfe, e_series)
+    parameters.sort(key=_most)
 
     view_apmlifiered(parameters, top)
 
