cff13f8d0cad86c00f423817e2e867d6a316646c
umedoblock <umedoblock@umetaro>
2017-11-03 15:28:15 +0900

    typo.

--- decide_language.py
+++ decide_language.py
@@ -7,7 +7,7 @@ import engineer_number
 # echo ${LANGUAGE}
 
 language_priority = engineer_number._decide_languages_order()
-print("show you language priority.")
+print("show your language priority.")
 print(language_priority)
 print("first language is {}".format(language_priority[0]))
 # $ export LANGUAGE=en:ja
