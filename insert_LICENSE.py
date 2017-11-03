1f7356bcf0d7c7e6fcb32eb149b68bf5718621c3
umedoblock <umedoblock@umetaro>
2017-11-03 16:47:56 +0900

    engineer_number 以下の python scirpt を見つけて再帰的に著作権情報を書く ようにした。
    著作権情報を格納する変数の名前を小文字(=license)から大文字(=LICENSE)に変更。

--- insert_LICENSE.py
+++ insert_LICENSE.py
@@ -1,4 +1,11 @@
-license = """# engineer_number module
+# engineer_number module
+#
+# Copyright (c) 2012-2017 梅濁酒(umedoblock)
+#
+# This software is released under the MIT License.
+# https://github.com/umedoblock/engineer_number
+
+LICENSE = """# engineer_number module
 #
 # Copyright (c) 2012-2017 梅濁酒(umedoblock)
 #
@@ -9,9 +16,9 @@ license = """# engineer_number module
 
 if __name__ == "__main__":
     import glob
-    for file_name in glob.glob("engineer_number/**/*.py"):
-#       print("file_name =", file_name)
+    for file_name in glob.glob("engineer_number/**/*.py", recursive=True):
+        print("file_name =", file_name)
         with open(file_name, "r") as f:
             codes = f.read()
         with open(file_name, "w") as f:
-            print(license + codes, file=f, end="")
+            print(LICENSE + codes, file=f, end="")
