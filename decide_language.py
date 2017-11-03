10a1ba47dce4866d6599507ee74d0eb70373148f
umedoblock <umedoblock@umetaro>
2017-11-03 15:28:03 +0900

    engineer_number 以下の I18N 用 method を使用する。

--- decide_language.py
+++ decide_language.py
@@ -1,38 +1,12 @@
 import os, re
 
-def get_default_languages():
-    languages = []
-    for language in ('LANGUAGE', 'LC_ALL', 'LC_MESSAGES', 'LANG'):
-        env = os.environ.get(language)
-        if not env:
-            print("{} is missing.".format(language))
-            continue
-        print("{}={}".format(language, env))
-        sp = env.split(':')
-        languages.extend(sp)
-    print()
-
-    if not languages:
-        languages.append('C')
-    return languages
-
-def decide_languages_order(hope=[]):
-    # first is overwrited by second.
-    # so should be hope=['lower-important', 'higher-important']
-    languages = get_default_languages()
-    if not re.search(r'ja', ",".join(languages)) or languages[0].startswith("en_"):
-        languages.insert(0, 'zannenenglish')
-    languages.extend(hope)
-
-    return languages
-
+import engineer_number
 # LANG=en_US.UTF-8
 # LANGUAGE=en_US:en
 # echo ${LANG}
 # echo ${LANGUAGE}
-# print(get_default_languages())
-# print(decide_languages_order())
-language_priority = decide_languages_order(hope=['lower-important', 'higher-important'])
+
+language_priority = engineer_number._decide_languages_order()
 print("show you language priority.")
 print(language_priority)
 print("first language is {}".format(language_priority[0]))
